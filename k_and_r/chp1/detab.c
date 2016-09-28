1d0ee24259ed4c9c6227383bf9466c1692b66783
jcalzaretta <jcalzare@sdf.org>
2016-09-27 21:37:54 -0500

    finish detab assignment

--- k_and_r/chp1/detab.c
+++ k_and_r/chp1/detab.c
@@ -12,33 +12,33 @@
 int getliner(char line[], int maxline);
 void tab(char line[], char output[], int size);
 int find_blanks(int column, int tabs);
+void printer(char text[], int length);
 
-main() {
+void main(void) {
   char line[MAXLINE];	/* line saved here */
-  char tabbed[MAXLINE];	/* modified tabbed line saved here */
+  char output[MAXLINE];	/* modified tabbed line saved here */
   int linesize;		/* size of input line */
 
   linesize = getliner(line, MAXLINE);	/* save the input  to line[], store size to linesize */
-  tab(line, tabbed, linesize);		/* tabulate line[] and save to tabbed[] */
+  tab(line, output, linesize);		/* tabulate line[] and save to tabbed[] */
+  printer(output, linesize);
 }
 
 int getliner(char input[], int lim) {
-  int c, i;
-
-  for (i = 0; i <= lim - 1 && (c = getchar()) != EOF; ++i)
-    printf("getting input");
+  int i, c = 0;
+  while((c = getchar()) != EOF) {
     input[i] = c;
-  input[i + 1] = '\0';
+    ++i;
+  }
   return i;
 }
 
-void tab(char input[], char output[], int size) {
-  printf("we are in the tab method");
+void tab(char input_ary[], char output[], int size) {
   int output_iterator, input_iterator, diff, i;
   output_iterator = input_iterator = 0;
-  
-  while (input_iterator <= size)
-    if (input[input_iterator] == '\t') {
+
+  while (input_iterator <= size) {
+    if (input_ary[input_iterator] == '\t') {
       diff = find_blanks(output_iterator, TABS);	/* calculate distance to next tab stop */
       for (i = 0; i <= diff; ++i)
 	output[output_iterator + i] = ' ';		/* insert spaces until next tab stop */
@@ -46,10 +46,21 @@ void tab(char input[], char output[], int size) {
       output_iterator += diff;
     }
     else {
-      output[output_iterator] = input[input_iterator];
+      output[output_iterator] = input_ary[input_iterator];
+      ++output_iterator;
+      ++input_iterator;
     }
+  }
 }
+
 
 int find_blanks(int column, int tabs) {
   return column % tabs;
+}
+
+void printer(char text[], int length) {
+  int i;
+  for(i = 0; i <= length; i++) {
+    printf("%c", text[i]);
+  }
 }
